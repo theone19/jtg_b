6364129e3451219457f5358059310bfa6cfcbedf
theone19 <thanasate@live.com>
2025-11-01 00:00:53 +0700

    add register and login

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { swagger } from "@elysiajs/swagger";
 import { staticPlugin } from "@elysiajs/static";
 import { jwt } from "@elysiajs/jwt";
-import { compare, hash } from "bcryptjs";
+// import bcrypt from "bcryptjs";
+import prisma from "./db";
 
 const rootApi = Bun.env.ROOT_API || "/api";
 
@@ -42,6 +43,86 @@ const app = new Elysia({ prefix: rootApi })
       return { error: "Invalid token" };
     }    
   })
+
+  // Register a new user
+  .post(
+    "/register",
+    async ({ body, set }) => {
+      const { email, userName, password, fullName } = body;
+
+      const existingUser = await prisma.appUser.findUnique({
+        where: { email },
+      });
+      if (existingUser) {
+        set.status = 409;
+        return { error: "User already exists" };
+      }
+
+      // const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await Bun.password.hash(password);
+      const newUser = await prisma.appUser.create({
+        data: {
+          email,
+          userName,
+          password: hashedPassword,
+          fullName,
+        },
+      });
+
+      return {
+        data: newUser,
+        message: "User created successfully",
+        success: true,
+      };
+    },
+    {
+      body: t.Object({
+        email: t.String({ format: "email" }),
+        userName: t.String(),
+        password: t.String({ minLength: 6 }),
+        fullName: t.String(),
+      }),
+    }
+  )
+
+  // User Login
+  .post(
+    "/login",
+    async ({ jwt, body, set }) => {
+      const { loginName, password } = body;
+
+      const user =
+        (await prisma.appUser.findUnique({
+          where: { email: loginName },
+        })) ||
+        (await prisma.appUser.findUnique({
+          where: { userName: loginName },
+        }));
+
+      if (!user) {
+        set.status = 401;
+        return { error: "Invalid email, userName or password" };
+      }
+
+      // console.log("User found:", user);      
+      // const isValid = await bcrypt.compare(password, user.password);
+      const isValid = await Bun.password.verify(password, user.password);
+      
+      if (!isValid) {
+        set.status = 401;
+        return { error: "Invalid email, userName or password" };
+      }
+
+      const token = await jwt.sign({ id: user.userId });
+      return { data: { token }, message: "Login successful", success: true };
+    },
+    {
+      body: t.Object({
+        loginName: t.String(),
+        password: t.String({ minLength: 6 }),
+      }),
+    }
+  )
 
   .get("/", () => "Hello Elysia")
   // .use(import("./user/user").then((mod) => mod.default))
